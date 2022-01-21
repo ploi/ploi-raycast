fa7db43ea536d331fa2df0474c8748dc1a8a100d
Dennis <dennissmink@gmail.com>
2022-01-21 08:50:15 +0100

    prettier

--- src/Server.tsx
+++ src/Server.tsx
@@ -142,7 +142,9 @@ const SingleServerView = ({
   const sshUser = preferences?.ploi_ssh_user?.value ?? "ploi";
   return (
     <List searchBarPlaceholder="Search Sites...">
-      <List.Section title={`Sites on server ${server.name} · ${sites.length} site(s)`}>
+      <List.Section
+        title={`Sites on server ${server.name} · ${sites.length} site(s)`}
+      >
         <SitesList server={server} sites={sites} />
       </List.Section>
       <List.Section title="Server Information">
